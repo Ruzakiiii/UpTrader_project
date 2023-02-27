4e0c4093f722213a01d569f815359b29b5b040d1
RuK <104256395+Ruzakiiii@users.noreply.github.com>
2023-02-27 18:12:17 +0300

    Update views.py

--- main_app/views.py
+++ main_app/views.py
@@ -10,7 +10,5 @@ def start(request):
 def one_item(request, id:int):
     items = All_items.objects.get(id=id)
 
-    categories = Categories.objects.all()
-
-    return render(request,'main_app/des.html', { 'item': items, 'categories': categories})
+    return render(request,'main_app/des.html', { 'item': items})
 
