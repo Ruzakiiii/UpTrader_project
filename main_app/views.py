672da0cc2d4eaa7e972f6bc187f3ec0a85d23bea
RuK <104256395+Ruzakiiii@users.noreply.github.com>
2023-02-27 18:21:11 +0300

    Update views.py

--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from .models import *
 
 def start(request):
-
     items = All_items.objects.all()
 
-    return render(request, 'main_app/index.html', {'item': items})
+    categories = Categories.objects.all()
+
+    return render(request, 'main_app/index.html', {'item': items, 'categories': categories })
 
 def one_item(request, id:int):
     items = All_items.objects.get(id=id)
